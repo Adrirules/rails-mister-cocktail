3816223639993f540b96f78fa7a0b2d2a58bdf05
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-02-16 11:31:09 +0100

    change the route of my HP

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+root "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:create, :index, :show, :new] do
     resources :doses, only: [:new, :create,]
